bf279649faf1503038b5e179c2a90672369c1111
azu <azuciao@gmail.com>
2020-03-26 13:50:11 +0900

    fix: use debug log

--- src/sql-string-search.ts
+++ src/sql-string-search.ts
@@ -4,27 +4,27 @@ import { walk } from "estree-walker";
 import globby from "globby";
 
 const Acorn = require("acorn");
-
+const debug = require("debug")("sql-string-search");
 export const search = async (globs: string[]) => {
     const files = await globby(globs, {
         gitignore: true,
-        ignore: ["**/node_modules/**"]
+        ignore: ["**/node_modules/**"],
     });
-    const results: { filePath: string; range: [number, number], content: string } [] = [];
-    files.forEach(filePath => {
+    const results: { filePath: string; range: [number, number]; content: string }[] = [];
+    files.forEach((filePath) => {
         try {
             const content = fs.readFileSync(filePath, "utf-8");
             const nodes = grep(content);
-            nodes.forEach(node => {
+            nodes.forEach((node) => {
                 const range = node.range as [number, number];
                 results.push({
                     filePath: filePath,
                     range: range,
-                    content: content.slice(range[0], range[1])
+                    content: content.slice(range[0], range[1]),
                 });
             });
         } catch (error) {
-            console.log("Parsing at", filePath, "Error", error);
+            debug("Parsing at", filePath, "Error", error);
         }
     });
     return results;
@@ -33,7 +33,7 @@ export const search = async (globs: string[]) => {
 const hasSQLLikeString = (node: ESTree.TemplateLiteral) => {
     const boundaryLine = 2;
     let count = 0;
-    node.quasis.forEach(queasi => {
+    node.quasis.forEach((queasi) => {
         const patterns = /SELECT|WHERE|FROM/g;
         if (patterns.test(queasi.value.raw)) {
             count++;
@@ -44,7 +44,7 @@ const hasSQLLikeString = (node: ESTree.TemplateLiteral) => {
 export const grep = (code: string) => {
     const AST = Acorn.parse(code, {
         ranges: true,
-        sourceType: "module"
+        sourceType: "module",
     });
     const resultNodes: ESTree.TemplateLiteral[] = [];
     walk(AST, {
@@ -56,7 +56,7 @@ export const grep = (code: string) => {
             if (hasSQLLikeString(templateLiteral)) {
                 resultNodes.push(templateLiteral);
             }
-        }
+        },
     });
     return resultNodes;
 };
